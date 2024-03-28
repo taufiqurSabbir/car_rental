90e1bb618abde12c0378a1dc8f373df546f31816
taufiqursabbir <taufiqur.sabbir11@gmail.com>
2024-03-29 00:46:24 +0600

    app initial page setup

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Car_Rental extends StatefulWidget {
+  const Car_Rental({super.key});
+
+  @override
+  State<Car_Rental> createState() => _Car_RentalState();
+}
+
+class _Car_RentalState extends State<Car_Rental> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
