848d87e94f98e0cfd569e9a4084bc55d12ccef01
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-03-29 16:06:02 +0600

    customer info page frontend added

--- lib/view/screens/customer_info.dart
+++ lib/view/screens/customer_info.dart
@@ -1,28 +1,247 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
-class Customer_Information extends StatefulWidget {
-  const Customer_Information({super.key});
+import '../utils/app_colors.dart';
+import '../widget/inputtitle.dart';
+import '../widget/screenTitle.dart';
+import 'VechicleInfo.dart';
+
+class CustomerInformation extends StatefulWidget {
+  const CustomerInformation({Key? key}) : super(key: key);
 
   @override
-  State<Customer_Information> createState() => _Customer_InformationState();
+  State<CustomerInformation> createState() => _CustomerInformationState();
 }
 
-class _Customer_InformationState extends State<Customer_Information> {
+class _CustomerInformationState extends State<CustomerInformation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
+        titleSpacing: 0,
+        toolbarHeight: 56, // Adjust the height as needed
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
+          icon: Icon(Icons.arrow_back_ios_new, color: Colors.black, size: 18),
           onPressed: () => Navigator.of(context).pop(),
         ),
-        title: Text("Back"),
+        title: Text(
+          "Back",
+          style: TextStyle(fontSize: 18),
+        ),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10.h,
+              ),
+              ScreenTitle(
+                title: 'Customer Information',
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey.shade300),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(top: 20, left: 15.0, right: 15),
+                    child: Column(
+                      children: [
+                        // first name input section start
 
-          ],
+                        InputTitle(
+                          title: 'First Name',
+                          isrequried: true,
+                        ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
+                        SizedBox(
+                          height: 35.h,
+                          child: TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(7.0),
+                                borderSide: BorderSide(
+                                  color: AppColors.primarycolor,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(7.0),
+                                borderSide: BorderSide(
+                                  color: Colors.grey.shade300,
+                                  width: 2.0,
+                                ),
+                              ),
+                            ),
+                            validator: (String? value) {
+                              if (value?.isEmpty ?? true) {
+                                return 'Enter your Reservation Id';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+
+                        //first name input end
+
+                        //last name input start
+
+                        InputTitle(
+                          title: 'Last Name',
+                          isrequried: true,
+                        ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
+                        SizedBox(
+                          height: 35.h,
+                          child: TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(7.0),
+                                borderSide: BorderSide(
+                                  color: AppColors.primarycolor,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(7.0),
+                                borderSide: BorderSide(
+                                  color: Colors.grey.shade300,
+                                  width: 2.0,
+                                ),
+                              ),
+                            ),
+                            validator: (String? value) {
+                              if (value?.isEmpty ?? true) {
+                                return 'Enter your Reservation Id';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+
+                        //last name input end
+
+                        //email input start
+
+                        InputTitle(
+                          title: 'Email',
+                          isrequried: true,
+                        ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
+                        SizedBox(
+                          height: 35.h,
+                          child: TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(7.0),
+                                borderSide: BorderSide(
+                                  color: AppColors.primarycolor,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(7.0),
+                                borderSide: BorderSide(
+                                  color: Colors.grey.shade300,
+                                  width: 2.0,
+                                ),
+                              ),
+                            ),
+                            validator: (String? value) {
+                              if (value?.isEmpty ?? true) {
+                                return 'Enter your Reservation Id';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+
+                        //email input end
+
+                        //phone input start
+
+                        InputTitle(
+                          title: 'Phone',
+                          isrequried: true,
+                        ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
+                        SizedBox(
+                          height: 35.h,
+                          child: TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(7.0),
+                                borderSide: BorderSide(
+                                  color: AppColors.primarycolor,
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(7.0),
+                                borderSide: BorderSide(
+                                  color: Colors.grey.shade300,
+                                  width: 2.0,
+                                ),
+                              ),
+                            ),
+                            validator: (String? value) {
+                              if (value?.isEmpty ?? true) {
+                                return 'Enter your Reservation Id';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15.h,
+                        ),
+
+                        //phone input end
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100.h,
+              ),
+              Center(
+                child: SizedBox(
+                    height: 30.h,
+                    width: 150.w,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Get.to(VechicleInfo());
+                        },
+                        child: Text('Next'))),
+              )
+            ],
+          ),
         ),
       ),
     );
